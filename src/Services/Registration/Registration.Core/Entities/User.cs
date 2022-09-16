a940748d1adb5ba15261672dadb3f181c8e32fe7
Victor <vicktorteixeira.7@gmail.com>
2022-09-16 18:21:01 -0300

    fix: Id is going to generate automatically

--- src/Services/Registration/Registration.Core/Entities/User.cs
+++ src/Services/Registration/Registration.Core/Entities/User.cs
@@ -5,7 +5,7 @@ namespace Registration.Core.Entities;
 
 public class User
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? Username { get; set; }
