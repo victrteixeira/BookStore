a0b9a78c2c130f92ef533d702c32d00c8b3e98bc
Victor <vicktorteixeira.7@gmail.com>
2022-09-24 12:52:35 -0300

    fix: Test Project's name renamed, because were causing conflict

---
+++ src/Tests/Catalog.Tests/Domain.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using Bogus.DataSets;
+global using Catalog.Core.Entities;
+global using Catalog.Core.Exceptions;
+global using Catalog.Core.Validators;
+global using FluentAssertions;
