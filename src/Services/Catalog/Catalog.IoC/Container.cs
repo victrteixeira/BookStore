8d09d4406fd19e921434794b394f4e07eeeba3ef
Victor <vicktorteixeira.7@gmail.com>
2022-09-28 16:51:46 -0300

    feat: Inversion Of Control project to organize dependency injection from Catalog API

---
+++ src/Services/Catalog/Catalog.IoC/Container.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Core.Interfaces;
+using Catalog.Infra.Database;
+using Catalog.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.IoC;
+
+public static class Container
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("CatalogConnection");
+
+        services.AddDbContext<CatalogContext>(opt => opt.UseSqlServer(connectionString));
+
+        services.AddScoped<IAuthorRepository, AuthorRepository>();
+        services.AddScoped<IBookRepository, BookRepository>();
+
+        return services;
+    }
+}
