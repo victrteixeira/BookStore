b4c0477e7a57d0200605bf09250f9680697b1e6c
Victor <vicktorteixeira.7@gmail.com>
2022-09-30 14:45:23 -0300

    feat: redis repository added for data caching

---
+++ src/Services/Catalog/Catalog.Infra/Interfaces/IRedisCacheRepository.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Infra.Interfaces;
+
+public interface IRedisCacheRepository
+{
+    Task<T?> GetAsync<T>(string key);
+    Task<T> SetAsync<T>(string key, T value);
+}
