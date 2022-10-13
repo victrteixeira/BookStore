c401cfd5d4f4c589b45759fe95b98529ba2dd5a2
Victor <vicktorteixeira.7@gmail.com>
2022-10-13 13:34:25 -0300

    refac: entity refactored with a new constructor and author and genre id made private with a method to update it

--- src/Services/Catalog/Catalog.Core/Entities/Book.cs
+++ src/Services/Catalog/Catalog.Core/Entities/Book.cs
@@ -4,16 +4,6 @@ namespace Catalog.Core.Entities;
 
 public sealed class Book : Base
 {
-    public Book(string name, int pages, decimal price, string language, string? publisher)
-    {
-        Name = name;
-        Pages = pages;
-        Price = price;
-        Language = language;
-        Publisher = publisher;
-        Validate();
-    }
-    
     public Book(string name, int pages, decimal price, string language, string? publisher, int authorId)
     {
         Name = name;
@@ -24,6 +14,18 @@ public sealed class Book : Base
         AuthorId = authorId;
         Validate();
     }
+    
+    public Book(string name, int pages, decimal price, string language, string? publisher, int authorId, int genreId)
+    {
+        Name = name;
+        Pages = pages;
+        Price = price;
+        Language = language;
+        Publisher = publisher;
+        AuthorId = authorId;
+        GenreId = genreId;
+        Validate();
+    }
 
     public int BookId { get; private set; }
     public string Name { get; private set; }
@@ -32,11 +34,29 @@ public sealed class Book : Base
     public string Language { get; private set; }
     public string? Publisher { get; private set; }
 
-    public int AuthorId { get; set; }
+    public int AuthorId { get; private set; }
     public Author Author { get; set; } = null!;
     
-    public int GenreId { get; set; }
+    public int GenreId { get; private set; }
     public Genre Genre { get; set; } = null!;
 
     public bool Validate() => base.Validate(new BookValidator(), this);
+
+    public int UpdateAuthorId(int authorId)
+    {
+        if (authorId < 1)
+            return 0;
+        
+        AuthorId = authorId;
+        return 1;
+    }
+
+    public int UpdateGenreId(int genreId)
+    {
+        if (genreId < 1)
+            return 0;
+
+        GenreId = genreId;
+        return 1;
+    }
 }
