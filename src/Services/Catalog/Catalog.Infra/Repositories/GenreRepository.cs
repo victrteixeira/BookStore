5febfa7056516cc14ffa96da67bedd88f9209c3c
Victor <vicktorteixeira.7@gmail.com>
2022-10-14 13:24:33 -0300

    feat: included query for existent genre

--- src/Services/Catalog/Catalog.Infra/Repositories/GenreRepository.cs
+++ src/Services/Catalog/Catalog.Infra/Repositories/GenreRepository.cs
@@ -1,12 +1,21 @@
 ﻿using Catalog.Core.Entities;
 using Catalog.Core.Interfaces;
 using Catalog.Infra.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Infra.Repositories;
 
 public class GenreRepository : BaseRepository<Genre>, IGenreRepository
 {
-    public GenreRepository(CatalogContext context) : base(context)
+    private readonly CatalogContext _context;
+    public GenreRepository(CatalogContext context) : base(context) => _context = context;
+
+    public async Task<Genre?> GetGenre(string genreName, string subgenre)
     {
+        return await _context.Genres
+            .AsNoTracking()
+            .Where(x => x.Name.ToLower() == genreName.Trim().ToLower() &&
+                        x.SubGenre.ToLower() == subgenre.Trim().ToLower())
+            .FirstOrDefaultAsync();
     }
 }
