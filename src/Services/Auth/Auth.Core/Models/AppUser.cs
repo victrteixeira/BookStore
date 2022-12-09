fe03137f92312a8a4e4c08063815b1216d00be16
Victor <vicktorteixeira.7@gmail.com>
2022-12-09 16:12:33 -0300

    feat: new properties added to appuser to improve user's information on database.

--- src/Services/Auth/Auth.Core/Models/AppUser.cs
+++ src/Services/Auth/Auth.Core/Models/AppUser.cs
@@ -4,4 +4,7 @@ namespace Auth.Core.Models;
 
 public class AppUser : IdentityUser
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public int Age { get; set; }
 }
